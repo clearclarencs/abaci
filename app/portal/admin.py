8b67732c5e0f8cf19e637e57c73b63287665e4cf
clearclarencs <jamesskipworth007@gmail.com>
2021-09-09 00:41:27 +0100

    Removed docker

--- app/portal/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import topic, clss, comment
-''' PORTAL APP, ADMIN, For loading databases into admin page to control data from there'''
-admin.site.register(topic)
-admin.site.register(clss) # Add these models to admin portal for admins to edit data
-admin.site.register(comment)
